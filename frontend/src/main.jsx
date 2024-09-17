cad7c4e672182614a8e3c3982f6c1b08e449b48f
Abidmo07 <ma.abid@esi-sba.dz>
2024-09-17 17:55:37 +0100

    wrapping app component under the provider to make it able to access to provider's values

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from '../Routes/AppRoutes.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { TodoProvider } from './TodoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <TodoProvider>
+        <Router>
       <AppRoutes />
     </Router>
+    </TodoProvider>
+  
   </StrictMode>,
 )
